9f83d515a44edbb73d057e74ce184dbd15556bf8
Kevin Mwangi <Kevinnmwangi@gmail.com>
2015-02-05 19:05:12 +0300

    adding highcharts widget

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ requirejs.config({
 	baseUrl: "lib",
 	paths: {
 		"customDash-js": "../src/js",
+		"customDash-widget": "../src/widgets",
+		"text": "bower-components/text",
 		"jquery": "bower-components/jquery/dist/jquery",
-		"mithril":"bower-components/mithril/mithril.min"
+		"mithril":"bower-components/mithril/mithril.min",
+		"charts":"../src/widgets/Barchart/barcharts",
+		"csv":"../src/widgets/csv/read-csv",
+		"highcharts":"bower-components/highcharts-release/"
 	},
 	shim: {
 		"Modernizr": {
@@ -11,13 +16,14 @@ requirejs.config({
 		}
 	}
 });
-require([ 'jquery', 'customDash-js/read-csv', 'mithril' ], 
-	function ( $, csv, mithril ) {
-	
-	jQuery(document).ready(function(){
-		//m.render(document.getElementById("outputTable"), m(".container"));
-		console.log('jQuery version: *-------------------------*'); // 1.9.0
-	});
+require([ 'jquery', 'Modernizr', 'charts', 'csv' ], function ( $, Modernizr, barcharts, csv ) {
+	 //barcharts.initialize();
+	 csv.initialize();
+//	jQuery(document).ready(function(){
+//		//m.module(document, app);
+//		//m.render(document.getElementById("outputTable"), m(".container"));
+//		//console.log('jQuery version: *-------------------------*'); // 1.9.0
+//	});
 	
  
 });
--- src/widgets/Barchart/barcharts.js
+++ src/widgets/Barchart/barcharts.js
@@ -1,148 +1,102 @@
-$(function () {
+//var app = {};
+//app.App = function(data){
+//    this.plotCfg = {
+//        chart: {
+//            renderTo: "plot"
+//        },
+//        series: [{
+//            name: 'USD to EUR',
+//            data: usdeur
+//        }]
+//    };
+//};
+//app.controller = function(){
+//    this.apk = new app.App();
+//    this.cfg = this.apk.plotCfg;
+//};
+//app.plotter = function(ctrl) {
+//    return function(elem,isin) {
+//        if(!isin) {
+//          m.startComputation();
+//          var chart = Highcharts.StockChart(ctrl.cfg);
+//          m.endComputation();
+//        }
+//    };
+//};
+//app.view = function(ctrl) {
+//    return  m("html", [ m("body", [
+//        m("#plot[style=height:400px]", {config: app.plotter(ctrl)}),
+//        //when I set breakpoint here I can see plot for a moment. It disappears when I resume normal script flow
+//        m("p", "some text after plot"),
+//        ]),
+//    ])
+//};
 
-    var gaugeOptions = {
 
-        chart: {
-            type: 'solidgauge'
+//if ( typeof define !== 'function' ) {
+//    var define = require( 'amdefine' )( module );
+//}
+
+define([ 'jquery', 'highcharts/highcharts', 'highcharts/highcharts-more', 'highcharts/modules/solid-gauge.src' ], function() {
+var initialize = function () {
+	console.log('jQuery version: *-------------------------*'); // 1.9.0
+
+	$('#container-speed').highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
         },
-
-        title: null,
-
-        pane: {
-            center: ['50%', '85%'],
-            size: '100%',
-            startAngle: -90,
-            endAngle: 90,
-            background: {
-                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
-                innerRadius: '40%',
-                outerRadius: '100%',
-                shape: 'arc'
-            }
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
         },
-
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
         tooltip: {
-            enabled: false
+            valueSuffix: '°C'
         },
-
-        // the value axis
-        yAxis: {
-            stops: [
-                //[0.1, '#55BF3B'], // green
-                //[0.5, '#DDDF0D'], // yellow
-                [0.9, '#DF5353'] // red
-            ],
-            lineWidth: 0,
-            minorTickInterval: null,
-            tickPixelInterval: 400,
-            tickWidth: 0,
-            title: {
-                y: -70
-            },
-            labels: {
-                y: 16
-            }
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
         },
-
-        plotOptions: {
-            solidgauge: {
-                dataLabels: {
-                    y: 5,
-                    borderWidth: 0,
-                    useHTML: true
-                }
-            }
-        }
-    };
-
-    // The speed gauge
-    $('#container-speed').highcharts(Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: 0,
-            max: 200,
-            title: {
-                text: 'Speed'
-            }
-        },
-
-        credits: {
-            enabled: false
-        },
-
         series: [{
-            name: 'Speed',
-            data: [80],
-            dataLabels: {
-                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
-                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
-                       '<span style="font-size:12px;color:silver">km/h</span></div>'
-            },
-            tooltip: {
-                valueSuffix: ' km/h'
-            }
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
+    });
+//	$(document).ready(function () {
+//
+////		m.render(document.getElementById("outputTable"), m(".container"));
+////		console.log('$.widget is defined? ' + Boolean($.widget));
+////		console.log('jQuery.widget is defined? ' + Boolean(jQuery.widget));
+//	});
+};
 
-    }));
-
-    // The RPM gauge
-    $('#container-rpm').highcharts(Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: 0,
-            max: 5,
-            title: {
-                text: 'RPM'
-            }
-        },
-
-        series: [{
-            name: 'RPM',
-            data: [1],
-            dataLabels: {
-                format: '<div style="text-align:center"><span style="font-size:25px;color:' +
-                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y:.1f}</span><br/>' +
-                       '<span style="font-size:12px;color:silver">* 1000 / min</span></div>'
-            },
-            tooltip: {
-                valueSuffix: ' revolutions/min'
-            }
-        }]
-
-    }));
-
-    // Bring life to the dials
-    setInterval(function () {
-        // Speed
-        var chart = $('#container-speed').highcharts(),
-            point,
-            newVal,
-            inc;
-
-        if (chart) {
-            point = chart.series[0].points[0];
-            inc = Math.round((Math.random() - 0.5) * 100);
-            newVal = point.y + inc;
-
-            if (newVal < 0 || newVal > 200) {
-                newVal = point.y - inc;
-            }
-
-            point.update(newVal);
-        }
-
-        // RPM
-        chart = $('#container-rpm').highcharts();
-        if (chart) {
-            point = chart.series[0].points[0];
-            inc = Math.random() - 0.5;
-            newVal = point.y + inc;
-
-            if (newVal < 0 || newVal > 5) {
-                newVal = point.y - inc;
-            }
-
-            point.update(newVal);
-        }
-    }, 2000);
-
+return {
+	initialize: initialize
+};
 
 });
